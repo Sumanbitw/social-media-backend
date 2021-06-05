4c41fec6586f71844be64028934a82a8045f2645
Sumanbitw <007sumanghosh@gmail.com>
2021-06-05 09:50:19 +0530

    initial setup

---
+++ api/router/user.router.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = require('../../app')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+var jwt = require('jsonwebtoken');
+require("dotenv/config")
+const User = require("../models/user")
+
+
+router.get('/signup',async(req, res, next) => {
+    try{
+    const allUser = await User.find()
+    res.status(200).json(allUser)
+    }catch(error){
+        res.status(500).json({
+            message : error
+        })
+    }
+})
+router.post("/signup", async (req, res, next) => {
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    const userExists = await User.findOne({ email : req.body.email })
+    if (userExists){
+        return res.json({ message : "User already exists" })
+    }else {
+        const user = new User({
+            _id : new mongoose.Types.ObjectId(),
+            name : req.body.name,
+            email : req.body.email,
+            password : hashedPassword,
+            })
+        }
+        try {    
+        const savedUser = await user.save()
+        const token = jwt.sign({
+            userId : savedUser._id,
+            email : savedUser.email
+        }, process.env.JWT_KEY, {expiresIn : "1d"})
+        console.log(savedUser)
+        res.status(201).json({message : "Account created ", token : token})
+        }catch(error) {
+        console.log(error)
+        res.status(500).json({
+            message : error
+        })   
+    }      
+    })
+
+     router.delete('/signup/:userId', async (req, res, next) => {
+         try{
+         const removeUser =  await User.remove({ _id : req.params.userId })
+         res.status(200).json(removeUser)
+        }catch(error){
+            res.status(500).json({
+                message : error
+            })
+        }    
+     })
+
+     router.post('/login',async (req, res, next) => {
+        const user = await User.findOne({ email : req.body.email })
+        !user && res.status(401).json({ message : "Auth login failed" })
+
+        const validatePassword = await bcrypt.compare(req.body.password, user.password)
+         !validatePassword && res.status(401).json({ message : "Wrong Password" })
+
+         try{
+            const token = jwt.sign({ 
+                userId : user._id,
+                email : user.email,
+            }, process.env.JWT_KEY, {expiresIn : "1d"})
+            res.status(200).json({ message : "Succesfully login", token : token})
+         }catch(error) {
+             res.status(500).json({
+                 message : error
+             })
+         }
+     })
+     
+module.exports = router
--- app.js
+++ app.js
@@ -1,11 +1,51 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require("dotenv/config")
 
-
-app.get('/', (req, res) => {
-    res.send("Sociana backend")
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser : true, useUnifiedTopology: true }, () => {
+    console.log("connected to DB")
 })
 
-app.listen(port)
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended :true}))
+app.use(express.json())
+app.use(cors())
 
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization" 
+    );
+    if(req.method === "OPTIONS"){
+        res.header('Access-Control-Allow-Methods', 'PUT', 'PATCH', 'DELETE', 'POST', 'GET')
+        return res.status(200).json({})
+    }
+    next()
+})
+
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message : "Sociana Backend"
+    })
+})
+app.use((req,res,next ) => {
+    const error = new Error("Not found")
+    res.status(404)
+    next(error)
+})
+
+app.use((error,req,res,next) => {
+    res.status(error.status || 500)
+    res.json({
+        error : {
+            message : error.message
+        }
+    })
+})
+
+module.exports = app
