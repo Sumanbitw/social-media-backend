5f465178a4f6c328bf018150c995a583a7b3775e
Sumanbitw <007sumanghosh@gmail.com>
2021-06-23 21:22:06 +0530

    feat : created user router

--- api/router/user.router.js
+++ api/router/user.router.js
@@ -1,83 +1,123 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = require('../../app')
-const router = express.Router()
-const bcrypt = require('bcryptjs')
-var jwt = require('jsonwebtoken');
-require("dotenv/config")
-const User = require("../model/user.model")
+const User = require("../model/user.model");
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
-
-router.get('/signup',async(req, res, next) => {
-    try{
-    const allUser = await User.find()
-    res.status(200).json(allUser)
-    }catch(error){
-        res.status(500).json({
-            message : error
-        })
+//update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
     }
-})
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can update only your account!");
+  }
+});
 
-router.post("/signup", async (req, res, next) => {
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(req.body.password, salt)
-    const userExists = await User.findOne({ email : req.body.email })
-    if (userExists){
-        return res.json({ message : "User already exists" })
-    }else {
-        const user = new User({
-            _id : new mongoose.Types.ObjectId(),
-            name : req.body.name,
-            email : req.body.email,
-            password : hashedPassword,
-            })
-        try {    
-        const savedUser = await user.save()
-        const token = jwt.sign({
-            userId : savedUser._id,
-            email : savedUser.email
-        }, process.env.JWT_KEY, {expiresIn : "1d"})
-        console.log(savedUser)
-        res.status(201).json({message : "Account created ", token : token})
-        }catch(error) {
-        console.log(error)
-        res.status(500).json({
-            message : error
-        }) 
-    }  
-   }      
-})
+//delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account!");
+  }
+});
 
-    router.delete('/signup/:userId', async (req, res, next) => {
-        try{
-        const removeUser =  await User.remove({ _id : req.params.userId })
-        res.status(200).json(removeUser)
-    }catch(error){
-        res.status(500).json({
-            message : error
-        })
-    }    
-    })
+//get a user
+router.get("/:id", async (req, res) => {
+  // const userId = req.query.userId;
+  // const username = req.query.username;
+  try {
+    // const user = userId
+      // ? await User.findById(userId)
+      // : await User.findOne({ username: username });
+    const user = await User.findById(req.params.id)
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-    router.post('/login',async (req, res, next) => {
-    const user = await User.findOne({ email : req.body.email })
-    !user && res.status(401).json({ message : "Auth login failed" })
+//get friends
+router.get("/friends/:userId", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    const friends = await Promise.all(
+      user.followings.map((friendId) => {
+        return User.findById(friendId);
+      })
+    );
+    let friendList = [];
+    friends.map((friend) => {
+      const { _id, username, profilePicture } = friend;
+      friendList.push({ _id, username, profilePicture });
+    });
+    res.status(200).json(friendList)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-    const validatePassword = await bcrypt.compare(req.body.password, user.password)
-        !validatePassword && res.status(401).json({ message : "Wrong Password" })
+//follow a user
 
-        try{
-        const token = jwt.sign({ 
-            userId : user._id,
-            email : user.email,
-        }, process.env.JWT_KEY, {expiresIn : "1d"})
-        res.status(200).json({ message : "Succesfully login", token : token})
-        }catch(error) {
-            res.status(500).json({
-                message : error
-            })
-        }
-    })
-    
-module.exports = router
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+        res.status(200).json("user has been followed");
+      } else {
+        res.status(403).json("you allready follow this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you cant follow yourself");
+  }
+});
+
+//unfollow a user
+
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { followings: req.params.id } });
+        res.status(200).json("user has been unfollowed");
+      } else {
+        res.status(403).json("you dont follow this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you cant unfollow yourself");
+  }
+});
+
+module.exports = router;
