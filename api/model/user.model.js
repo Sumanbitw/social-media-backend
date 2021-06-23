062894607acf093294a9bf39c5c75bcc2ebb983f
Sumanbitw <007sumanghosh@gmail.com>
2021-06-23 21:19:56 +0530

    feat : created user model

--- api/model/user.model.js
+++ api/model/user.model.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose')
 
 const userSchema  = mongoose.Schema({
-    _id : mongoose.Schema.Types.ObjectId,
     name : { type :String, required : true },
     email : { type : String, required : true },
-    password : { type : String, required : true }
-})
+    password : { type : String, required : true },
+    profileImage : { type : String, default : "" },
+    coverImage : { type : String, default : ""},
+    followers : { type : Array, default : [] },
+    followings : { type : Array, default : [] },    
+    isAdmin : { type : Boolean, default : false },
+    description : { type : String, default : "" },
+    city : { type : String, default : "" }
+}, { timestamps : true }
+)
 
 module.exports = mongoose.model('User', userSchema)
