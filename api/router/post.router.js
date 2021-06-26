bb64339fb948cd9e7f48429e792a3cd1d9e63a68
Sumanbitw <007sumanghosh@gmail.com>
2021-06-26 23:41:37 +0530

    refactor

--- api/router/post.router.js
+++ api/router/post.router.js
@@ -18,7 +18,7 @@ router.post("/",verify, async (req, res) => {
 router.put("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-    if (post.userId === req.body.userId) {
+    if (post.user === req.body.user) {
       await post.updateOne({ $set: req.body });
       res.status(200).json("the post has been updated");
     } else {
@@ -33,7 +33,7 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-    if (post.userId === req.body.userId) {
+    if (post.user === req.body.user) {
       await post.deleteOne();
       res.status(200).json("the post has been deleted");
     } else {
@@ -48,11 +48,11 @@ router.delete("/:id", async (req, res) => {
 router.put("/:id/like", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-    if (!post.likes.includes(req.body.userId)) {
-      await post.updateOne({ $push: { likes: req.body.userId } });
+    if (!post.likes.includes(req.body.user)) {
+      await post.updateOne({ $push: { likes: req.body.user } });
       res.status(200).json("The post has been liked");
     } else {
-      await post.updateOne({ $pull: { likes: req.body.userId } });
+      await post.updateOne({ $pull: { likes: req.body.user } });
       res.status(200).json("The post has been disliked");
     }
   } catch (err) {
@@ -75,10 +75,10 @@ router.get("/:id", async (req, res) => {
 router.get("/timeline/:userId", async (req, res) => {
   try {
     const currentUser = await User.findById(req.params.userId);
-    const userPosts = await Post.find({ userId: currentUser._id });
+    const userPosts = await Post.find({ user: currentUser._id });
     const friendPosts = await Promise.all(
       currentUser.followings.map((friendId) => {
-        return Post.find({ userId: friendId });
+        return Post.find({ user: friendId });
       })
     );
     res.status(200).json(userPosts.concat(...friendPosts));
@@ -91,8 +91,8 @@ router.get("/timeline/:userId", async (req, res) => {
 
 router.get("/profile/:username", async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.params.username });
-    const posts = await Post.find({ userId: user._id });
+    const user = await User.findOne({ name: req.params.username });
+    const posts = await Post.find({ user: user._id });
     res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(err);
